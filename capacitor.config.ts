a35b176cdd7828a3d4e4cfbc8c50bff1ac838510
Shagun Bandi <shagunamitbandi@gmail.com>
2025-08-06 23:26:06 +0200

    Added ios and android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tales.app',
+  appName: 'Tales',
+  webDir: 'dist'
+};
+
+export default config;
