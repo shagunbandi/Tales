c19a841300b456b94abfce1b68279ce37b70b84e
Shagun Bandi <shagunamitbandi@gmail.com>
2025-08-06 23:48:15 +0200

    Icons

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,20 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.tales.app',
   appName: 'Tales',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      backgroundColor: '#1f2937',
+      androidScaleType: 'CENTER_CROP',
+      showSpinner: false,
+      splashFullScreen: true,
+      splashImmersive: true
+    }
+  }
 };
 
 export default config;
