82986c553531b0ca39adf612e9d95e0859549eda
Shagun Bandi <shagunamitbandi@gmail.com>
2025-08-17 12:33:30 +0200

    No need for iOS and Android stuff right now

--- cypress/support/e2e.js
+++
@@ -1,7 +0,0 @@
-// Custom Cypress commands
-Cypress.Commands.add("getByTestId", (testId, ...args) => {
-  return cy.get(`[data-testid="${testId}"]`, ...args);
-});
-
-// Ensure fixtures are available
-// No additional setup needed
